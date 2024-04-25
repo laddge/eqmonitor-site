21b266a5c1fc31f4c7cbcac1e98f5535a0a1f30f
YumNumm <onory0917@gmail.com>
2024-04-25 20:34:25 +0900

    remove unused docsaurus files

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require("/Users/r_onoue/dev/github.com/YumNumm/eqmonitor-docs/node_modules/.pnpm/infima@0.2.0-alpha.43/node_modules/infima/dist/css/default/default.css"),
-  require("/Users/r_onoue/dev/github.com/YumNumm/eqmonitor-docs/node_modules/.pnpm/@docusaurus+theme-classic@3.0.0_@types+react@18.2.39_react-dom@18.2.0_react@18.2.0_typescript@5.2.2/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/Users/r_onoue/dev/github.com/YumNumm/eqmonitor-docs/node_modules/.pnpm/@docusaurus+theme-classic@3.0.0_@types+react@18.2.39_react-dom@18.2.0_react@18.2.0_typescript@5.2.2/node_modules/@docusaurus/theme-classic/lib/nprogress"),
-  require("/Users/r_onoue/dev/github.com/YumNumm/eqmonitor-docs/src/css/custom.css"),
-];
